sipwise/rate-o-mat

commit e6809334e79308016a800c028860bf28b547b3da

TT#19707 Fix perlcritic warnings
Change-Id: Ib96ebf4ea392bc13037bebe4034b25571eb43d9d

rate-o-mat.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl -w
 use lib '/usr/share/ngcp-rate-o-mat';
 use strict;
+use warnings;
+
 use DBI;
 use POSIX qw(setsid mktime);
 use Fcntl qw(LOCK_EX LOCK_NB SEEK_SET);
@@ -13,7 +15,7 @@ use List::Util qw(shuffle);
 
 # constants: ###########################################################
 
-$0 = 'rate-o-mat';
+$0 = 'rate-o-mat'; ## no critic (Variables::RequireLocalizedPunctuationVars)
 my $fork = $ENV{RATEOMAT_DAEMONIZE} // 1;
 my $PID;
 my $pidfile = '/var/run/rate-o-mat.pid';
@@ -490,15 +492,16 @@ EOS
 EOS
 	) or FATAL "Error preparing panel billing info statement: ".$billdbh->errstr;
 
-	$sth_lnp_number = $billdbh->prepare("
+	$sth_lnp_number = $billdbh->prepare(<<EOS
 		SELECT lnp_provider_id
 		  FROM billing.lnp_numbers
 		 WHERE ? LIKE CONCAT(number,'%')
 		   AND (start <= FROM_UNIXTIME(?) OR start IS NULL)
 		   AND (end > FROM_UNIXTIME(?) OR end IS NULL)
-	".       join(", ", "ORDER BY LENGTH(number) DESC", @lnp_order_by) ."
-				 LIMIT 1
-	") or FATAL "Error preparing LNP number statement: ".$billdbh->errstr;
+EOS
+		. join(", ", "ORDER BY LENGTH(number) DESC", @lnp_order_by) .
+		"LIMIT 1"
+	) or FATAL "Error preparing LNP number statement: ".$billdbh->errstr;
 
 	$sth_profile_info = $billdbh->prepare(
 		"SELECT id, source, destination, ".
@@ -577,19 +580,17 @@ EOS
 
 	if ($split_peak_parts) {
 		my @exclude_fragment_fields = qw(start_time duration is_fragmented);
-		my %exclude_fragment_fields = map {$_,1} @exclude_fragment_fields;
+		my %exclude_fragment_fields = map { $_ => 1 } @exclude_fragment_fields;
 		my @fragment_fields = grep {!$exclude_fragment_fields{$_}} @cdr_fields;
 		$sth_create_cdr_fragment = $acctdbh->prepare(
-			"INSERT INTO accounting.cdr
-				    (".
-				    join(',', @fragment_fields, @exclude_fragment_fields).
-				    ")
-			      SELECT ".
-				    join(',', @fragment_fields).",
-				    start_time + ?,duration - ?,1
-				FROM accounting.cdr
-			       WHERE id = ? AND rating_status = 'unrated'
-			") or FATAL "Error preparing create cdr fragment statement: ".$acctdbh->errstr;
+			"INSERT INTO accounting.cdr (".
+			join(',', @fragment_fields, @exclude_fragment_fields).
+			") SELECT ".
+			join(',', @fragment_fields).",".
+			"start_time + ?,duration - ?,1".
+			"FROM accounting.cdr".
+			"WHERE id = ? AND rating_status = 'unrated'"
+		) or FATAL "Error preparing create cdr fragment statement: ".$acctdbh->errstr;
 	}
 
 	$sth_get_cbalances = $billdbh->prepare(
@@ -1941,7 +1942,7 @@ sub get_call_cost {
 			DEBUG "add init rate $rate per sec to costs";
 		} else {
 			last if $split_peak_parts and defined($$r_onpeak) and $$r_onpeak != $onpeak
-                                and !defined $cdr->{rating_duration};
+                                and not defined $cdr->{rating_duration};
 
 			$interval = $onpeak == 1 ?
 				$r_profile_info->{on_follow_interval} : $r_profile_info->{off_follow_interval};
@@ -2768,7 +2769,10 @@ sub main {
 	daemonize($pidfile)
 		if($fork == 1);
 
-	$SIG{TERM} = $SIG{INT} = $SIG{QUIT} = $SIG{HUP} = \&signal_handler;
+	local $SIG{TERM} = \&signal_handler;
+	local $SIG{INT} = \&signal_handler;
+	local $SIG{QUIT} = \&signal_handler;
+	local $SIG{HUP} = \&signal_handler;
 
 	if ($maintenance_mode eq 'yes') {
 		while (!$shutdown) {

t/Utils/Api.pm
@@ -1,6 +1,8 @@
 package Utils::Api;
 
 use strict;
+use warnings;
+
 use LWP::UserAgent qw();
 use JSON qw();
 use Test::More;
@@ -217,7 +219,7 @@ sub _create_item {
 			diag(JSON::from_json($res->decoded_content)->{message});
 		};
 	}
-	return undef;
+	return;
 }
 
 sub update_item {
@@ -401,7 +403,7 @@ sub check_interval_history {
 	my $limit = '';
 	my $ok = 1;
 	$page //= 1;
-	$rows //= 10,
+	$rows //= 10;
 	my @intervals;
 	$limit = '&start=' . DateTime::Format::ISO8601->parse_datetime($limit_dt) if defined $limit_dt;
 	my $nexturi = $uri.'/api/balanceintervals/'.$customer_id.'/?page='.$page.'&rows='.$rows.'&order_by_direction=asc&order_by=start'.$limit;
@@ -560,7 +562,7 @@ sub _get_interval_start {
 	} elsif ('topup_interval' eq $start_mode) {
 		return $ctime->clone; #->truncate(to => 'day');
 	}
-	return undef;
+	return;
 }
 
 sub _add_interval {
@@ -585,7 +587,7 @@ sub _add_interval {
 		}
 		return $to;
 	}
-	return undef;
+	return;
 }
 
 sub _last_day_of_month {
@@ -796,6 +798,8 @@ sub cartesian_product {
     return 0 if @C == 0; # Empty product
 
     @C == grep {ref eq 'ARRAY'} @C or die("Arrays of things required by cartesian");
+
+    ## no critic (ClassHierarchies::ProhibitOneArgBless)
 
     # Generate each cartesian product when there are no prior cartesian products.
 

t/Utils/Rateomat.pm
@@ -1,8 +1,9 @@
 package Utils::Rateomat;
 
+use strict;
+use warnings;
 use threads 'exit' => 'threads_only';
 
-use strict;
 use DBI;
 use Test::More;
 #use IPC::System::Simple qw(capturex);
@@ -41,6 +42,7 @@ our @EXPORT_OK = qw(
 	get_cdr_relation_data
 );
 
+## no critic (Variables::RequireLocalizedPunctuationVars)
 $ENV{RATEOMAT_BILLING_DB_USER} //= 'root';
 $ENV{RATEOMAT_PROVISIONING_DB_USER} //= 'root';
 $ENV{RATEOMAT_ACCOUNTING_DB_USER} //= 'root';
@@ -330,7 +332,7 @@ sub decimal_to_string {
 	if (defined $value) {
         return sprintf('%6f',$value);
     } else {
-		return undef;
+		return;
 	}
 }
 
@@ -433,7 +435,7 @@ sub _create_cdr {
 		my $id = _insert($dbh,'accounting.cdr',\%values);
 		return _get_cdr($dbh,$id,1) if ok($id,'cdr id '.$id.' created');;
 	}
-	return undef;
+	return;
 }
 
 sub _create_prepaid_costs_cdr {

t/rateomat-01-run.t
@@ -1,5 +1,6 @@
 
 use strict;
+use warnings;
 
 use Utils::Api qw();
 use Utils::Rateomat qw();

t/rateomat-05-parallel.t
@@ -1,5 +1,6 @@
 
 use strict;
+use warnings;
 
 use Utils::Api qw();
 use Utils::Rateomat qw();

t/rateomat-10-prepaid-costs.t
@@ -1,4 +1,5 @@
 use strict;
+use warnings;
 
 use Utils::Api qw();
 use Utils::Rateomat qw();

t/rateomat-20-basic-onnet.t
@@ -1,4 +1,5 @@
 use strict;
+use warnings;
 
 use Utils::Api qw();
 use Utils::Rateomat qw();

t/rateomat-25-basic-offnet.t
@@ -1,4 +1,5 @@
 use strict;
+use warnings;
 
 use Utils::Api qw();
 use Utils::Rateomat qw();

commit 92e7e5f92d12173bb841dccbb6f857854c31774b

TT#21750 Restore missing spaces in SQL queries
Fixes: commit e6809334e79308016a800c028860bf28b547b3da
Change-Id: I5a49a10e2d3d1e2d9d12d23b25af765923d4b2e1

rate-o-mat.pl
@@ -500,7 +500,7 @@ EOS
 		   AND (end > FROM_UNIXTIME(?) OR end IS NULL)
 EOS
 		. join(", ", "ORDER BY LENGTH(number) DESC", @lnp_order_by) .
-		"LIMIT 1"
+		" LIMIT 1"
 	) or FATAL "Error preparing LNP number statement: ".$billdbh->errstr;
 
 	$sth_profile_info = $billdbh->prepare(
@@ -586,9 +586,9 @@ EOS
 			"INSERT INTO accounting.cdr (".
 			join(',', @fragment_fields, @exclude_fragment_fields).
 			") SELECT ".
-			join(',', @fragment_fields).",".
-			"start_time + ?,duration - ?,1".
-			"FROM accounting.cdr".
+			join(',', @fragment_fields). ", " .
+			"start_time + ?,duration - ?,1 " .
+			"FROM accounting.cdr " .
 			"WHERE id = ? AND rating_status = 'unrated'"
 		) or FATAL "Error preparing create cdr fragment statement: ".$acctdbh->errstr;
 	}