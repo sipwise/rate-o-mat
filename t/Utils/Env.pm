a7c58494cab19e707c293ce4d8e98912e32974c4
Rene Krenn <rkrenn@sipwise.com>
2018-01-03 20:53:42 +0100

    TT#29755 free cash testcase WIP
    Change-Id: I2be104b5770c7f68f334d7d6b2867cb5383607cf

---
+++ t/Utils/Env.pm
@@ -0,0 +1,21 @@
+package Utils::Env;
+
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+    $DATETIME_TIMEZONE
+);
+
+$ENV{RATEOMAT_BILLING_DB_HOST} = '192.168.0.84';
+$ENV{RATEOMAT_PROVISIONING_DB_HOST} = '192.168.0.84';
+$ENV{RATEOMAT_ACCOUNTING_DB_HOST} = '192.168.0.84';
+$ENV{RATEOMAT_CONNECTION_TIMEZONE} = '+0:00'; #'UTC'; #vagrant SYSETM timezone is "Etc/UTC"
+
+$ENV{CATALYST_SERVER} = 'https://127.0.0.1:1443';
+
+our $DATETIME_TIMEZONE = undef; #'UTC';
+
+1;
