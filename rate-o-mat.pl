sipwise/rate-o-mat

commit e742185f647a4ecfc430f81aad15546b39777015

TT#26264 Create a pidfile even if we do not daemonize
Do this for now to make monit happy even when running from systemd.

Change-Id: I46bdc523d8dedd46cf20c0d106d8fc5642c4bff9

rate-o-mat.pl
@@ -18,7 +18,6 @@ use Storable qw(dclone);
 
 $0 = 'rate-o-mat'; ## no critic (Variables::RequireLocalizedPunctuationVars)
 my $fork = $ENV{RATEOMAT_DAEMONIZE} // 1;
-my $PID;
 my $pidfile = '/var/run/rate-o-mat.pid';
 my $type = 'call';
 my $loop_interval = ((defined $ENV{RATEOMAT_LOOP_INTERVAL} && $ENV{RATEOMAT_LOOP_INTERVAL}) ? int $ENV{RATEOMAT_LOOP_INTERVAL} : 10);
@@ -2840,24 +2839,45 @@ RATING_DURATION_FOUND:
 
 }
 
+sub create_pidfile {
+
+	my $pidfh;
+
+	open $pidfh, '>>', $pidfile or FATAL "Can't open '$pidfile' for writing: $!\n";
+	flock($pidfh, LOCK_EX | LOCK_NB) or FATAL "Unable to lock pidfile '$pidfile': $!\n";
+
+	return $pidfh;
+
+}
+
+sub write_pidfile {
+
+	my $pidfh = shift;
+
+	seek $pidfh, 0, SEEK_SET;
+	truncate $pidfh, 0;
+	printflush $pidfh "$$\n";
+
+}
+
 sub daemonize {
 
 	my $pidfile = shift;
+	my $pidfh;
 
 	chdir '/' or FATAL "Can't chdir to /: $!\n";
 	open STDIN, '<', '/dev/null' or FATAL "Can't read /dev/null: $!\n";
 	open STDOUT, ">", "/dev/null" or FATAL "Can't open /dev/null: $!\n";
 	open STDERR, ">", "/dev/null" or FATAL "Can't open /dev/null: $!\n";
-	open $PID, ">>", "$pidfile" or FATAL "Can't open '$pidfile' for writing: $!\n";
-	flock($PID, LOCK_EX | LOCK_NB) or FATAL "Unable to lock pidfile '$pidfile': $!\n";
+	$pidfh = create_pidfile($pidfile);
 	defined(my $pid = fork) or FATAL "Can't fork: $!\n";
 	exit if $pid;
 	setsid or FATAL "Can't start a new session: $!\n";
-	seek $PID, 0, SEEK_SET;
-	truncate $PID, 0;
-	printflush $PID "$$\n";
+	write_pidfile($pidfh);
 	open STDOUT, "|-", "logger -s -t $log_ident" or FATAL "Can't open logger output stream: $!\n";
 	open STDERR, '>&STDOUT' or FATAL "Can't dup stdout: $!\n";
+
+	return $pidfh;
 
 }
 
@@ -2877,12 +2897,17 @@ sub debug_rating_time {
 }
 
 sub main {
+	my $pidfh;
 
 	openlog($log_ident, $log_opts, $log_facility)
 		or die "Error opening syslog: $!\n";
 
-	daemonize($pidfile)
-		if($fork == 1);
+	if ($fork != 0) {
+		$pidfh = daemonize($pidfile);
+	} else {
+		$pidfh = create_pidfile($pidfile);
+		write_pidfile($pidfh);
+	}
 
 	local $SIG{TERM} = \&signal_handler;
 	local $SIG{INT} = \&signal_handler;
@@ -3112,6 +3137,6 @@ sub main {
 	$provdbh and $acctdbh->disconnect;
 	$dupdbh and $dupdbh->disconnect;
 	closelog;
-	close $PID;
+	close $pidfh;
 	unlink $pidfile;
 }

commit e3f3ec6bce887a881929eeaf51e1acdc74e34467

TT#26264 Make it possible to disable pidfile creation
For the testsuite we are setting no daemonization, which was previously
disabling pidfile creation, which hardcodes a pathname that requires
root privileges.

Make it possible to specify the pidfile name via a new environment
variable, and to disable it by passing an empty string.

Change-Id: Idcfa7196059c5223452c5ca655ff3fb9c5c9ef88
Fixes: commit e742185f647a4ecfc430f81aad15546b39777015

rate-o-mat.pl
@@ -18,7 +18,7 @@ use Storable qw(dclone);
 
 $0 = 'rate-o-mat'; ## no critic (Variables::RequireLocalizedPunctuationVars)
 my $fork = $ENV{RATEOMAT_DAEMONIZE} // 1;
-my $pidfile = '/var/run/rate-o-mat.pid';
+my $pidfile = $ENV{RATEOMAT_PIDFILE} // '/var/run/rate-o-mat.pid';
 my $type = 'call';
 my $loop_interval = ((defined $ENV{RATEOMAT_LOOP_INTERVAL} && $ENV{RATEOMAT_LOOP_INTERVAL}) ? int $ENV{RATEOMAT_LOOP_INTERVAL} : 10);
 my $debug = ((defined $ENV{RATEOMAT_DEBUG} && $ENV{RATEOMAT_DEBUG}) ? int $ENV{RATEOMAT_DEBUG} : 0);
@@ -2904,7 +2904,7 @@ sub main {
 
 	if ($fork != 0) {
 		$pidfh = daemonize($pidfile);
-	} else {
+	} elsif (length $pidfile) {
 		$pidfh = create_pidfile($pidfile);
 		write_pidfile($pidfh);
 	}

t/Utils/Rateomat.pm
@@ -51,6 +51,7 @@ $ENV{RATEOMAT_ACCOUNTING_DB_USER} //= 'root';
 
 $ENV{RATEOMAT_DEBUG} //= 1;
 $ENV{RATEOMAT_DAEMONIZE} = 0;
+$ENV{RATEOMAT_PIDFILE} = '';
 my $rateomat_timeout = 5;
 my $rateomat_pl = $ENV{RATEOMAT_PL} // '../rate-o-mat.pl';
 