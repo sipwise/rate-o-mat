bc5cdc8de79466f2b649f2c11d66201347b523ab
Rene Krenn <rkrenn@sipwise.com>
2018-04-18 12:20:22 +0200

    TT#35815 do not update balances if no prepaid_costs record
    + see https://gerrit.mgm.sipwise.com/#/c/20083
    
    Change-Id: I119f23adbb925e88f74e42d0847afcd6db333149
    (cherry picked from commit 21f7fe898a5b601cace33baa1f74b89a23d887cb)

--- rate-o-mat.pl
+++ rate-o-mat.pl
@@ -53,6 +53,9 @@ my $split_peak_parts = ((defined $ENV{RATEOMAT_SPLIT_PEAK_PARTS} && $ENV{RATEOMA
 # update subscriber prepaid attribute value upon profile mapping updates:
 my $update_prepaid_preference = 1;
 
+# don't update balance of prepaid contracts, if no prepaid_costs record is found (re-rating):
+my $prepaid_update_balance = 0;
+
 # control writing cdr relation data:
 # disable it for now until this will be limited to prepaid contracts,
 # as it produces massive amounts of zeroed or unneeded data.
@@ -2375,8 +2378,10 @@ sub get_customer_call_cost {
 			# maybe another rateomat was faster and already processed+deleted it?
 			# in that case we should bail out here.
 			WARNING "no prepaid cost record found for call ID $cdr->{call_id}, applying calculated costs";
-			update_contract_balance(\@balances)
-				or FATAL "Error updating ".$dir."customer contract balance\n";
+			if ($prepaid_update_balance) {
+				update_contract_balance(\@balances)
+					or FATAL "Error updating ".$dir."customer contract balance\n";
+			}
 			$$r_cost = $real_cost;
 			$cdr->{$dir."customer_cash_balance_after"} = $snapshot_bal->{cash_balance};
 			$cdr->{$dir."customer_free_time_balance_after"} = $snapshot_bal->{free_time_balance};
